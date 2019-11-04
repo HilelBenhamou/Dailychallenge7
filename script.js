2cab4a07ed81c3afe1cee4a2e9e5864c61766dbc
HilelBenhamou <hilelbenhamou@hotmail.fr>
2019-11-04 20:17:06 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+//1 
+array.shift();
+//2
+ array.sort();
+ //3
+ array.push("Kiwi");
+ //4
+array.splice(1);
+ //5
+array.reverse();
